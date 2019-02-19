3a3e45d0c6fa3c3bb6841b6ad3c086b0c8a98b22
kenzo44 <kenzo44@users.noreply.github.com>
2019-02-19 14:46:33 -0700

    Redesigned UI

--- platforms/browser/www/assets/js/scripts.js
+++ platforms/browser/www/assets/js/scripts.js
@@ -1,19 +1,29 @@
-var app = new Framework7 ({
-    root: "#app",  //this is the app element
+var app = new Framework7({
+    root: "#app", //this is the app element
+
+    toolbar: {
+        hideOnPageScroll: true,
+    },
+    view: {
+        animate: false,
+    },
+
     routes: [
         {
             path: '/',
+            id: 'tab-1',
             url: './index.html'
         },
         {
             path: '/page2/',
+            id: 'tab-2',
             url: 'pages/page2.html'
         },
         {
             path: '/page3/',
             url: 'pages/page3.html'
         }
-    
+
     ]
 })
 
